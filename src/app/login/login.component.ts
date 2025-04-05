f6d8954072ee621a439c7a7b8e46b63c29ac84b4
Shyam-Lal <pvshyamlal4@gmail.com>
2025-04-05 16:33:17 +0530

    header part done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -32,9 +32,15 @@ export class LoginComponent {
         this.isSuccess = true;
         this.message = 'Login successful!';
         this.messageService.setMessage(this.message);
-        this.authService.setToken(response.token); // ✅ Save using service
-        this.router.navigate(['/home']);
-        console.log('Navigating to home');
+  
+        localStorage.setItem('token', response.token); // Save token
+        console.log('✅ Token saved to localStorage');
+  
+        // Force route reload to trigger Header update
+        this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+          this.router.navigate(['/home']);
+        });
+  
       },
       error => {
         console.error('Login failed', error);
